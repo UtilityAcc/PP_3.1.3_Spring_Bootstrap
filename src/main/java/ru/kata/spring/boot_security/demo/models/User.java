192af72b301b94f15cc88c403bd68e472aeeefa9
UtilityAcc <96780372+UtilityAcc@users.noreply.github.com>
2022-07-17 06:44:21 +0300

    Update User.java

--- src/main/java/ru/kata/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -19,10 +19,10 @@ public class User {
     @Column(name = "password")
     private String password;
 
-    @Column(name = "first_Name")
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "second_Name")
+    @Column(name = "second_name")
     private String secondName;
 
     @Column(name = "age")
